cc5c80373ba598ab97d0594ff23bbde0ce395a37
jigyasudhingra <68184200+jigyasudhingra@users.noreply.github.com>
2022-09-02 19:19:52 +0530

    Remove the static

--- ecom/urls.py
+++ ecom/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
